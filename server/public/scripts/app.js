909ed85bf67a393d69c7cfef95f5d86b127aeb6b
apeloza <apeloza@alumni.uwo.ca>
2016-05-09 11:44:26 -0500

    Update app.js

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -12,33 +12,37 @@ $(document).ready(function() {
                 $('.buttonrow').append('<button class="pbtn" id ="' + i + '">' + i + '</button>');
             }
 
+            //This timer cycles the displayed data every 10 seconds, unless a user clicks manually.
+            var timerID = setInterval(moveData, 10000);
+
             //moveData is called, setting our targeter to array slot 0.
             moveData(0);
-
-            //This timer will automatically advance the index if nothing is clicked.
-            var timerID = setInterval(moveData, 10000);
 
             //This listens for a click on next or previous
             $('.move').on('click', function() {
 
                 //If you clicked previous, moveData will move back one. If you clicked next, moveData will move forward one.
                 moveData($(this).data('move'));
+
             });
 
-            //This listens for a click directly on an index box. It will use moveData to move to that box.
+            //This listens for a click directly on an index box.
             $('.buttonrow').on('click', '.pbtn', function() {
-                moveData(Number($(this).attr('id')));
+
+                //The currentIndex is set to be = to the box index.
+                currentIndex = Number($(this).attr('id'));
+
+                //The red coloration is removed from the previous chosen index, and then applied to the new one.
+                $('.buttonrow').find('.chosen').removeClass('chosen');
+                $('#' + currentIndex).addClass('chosen');
+                 //The new data is called to be displayed on the DOM with appendPerson.
+                appendPerson();
             });
 
             //This function moves the red index marker. If no argument is supplied, it sets direction to 1.
             function moveData(direction = 1) {
 
-                //If currentIndex was altered by clicking on an index, currentIndex is set to be = to that index. If next or prev was clicked, the change is incremental.
-                if (direction > 1) {
-                    currentIndex = direction;
-                } else {
-                    currentIndex += direction;
-                }
+              currentIndex += direction;
 
                 //These if statements make sure that currentIndex will never be set to an illegal value.
                 if (currentIndex < 0) {
@@ -47,12 +51,11 @@ $(document).ready(function() {
                     currentIndex = 0;
                 }
 
-                //The marker is moved, and then appendPerson is called. Finally, the timer is reset.
+                //The marker is moved, and then appendPerson is called.
                 $('.buttonrow').find('.chosen').removeClass('chosen');
                 $('#' + currentIndex).addClass('chosen');
                 appendPerson();
-                clearInterval(timerID);
-                timerId = setInterval(moveData, 10000);
+
 
             }
 
@@ -66,6 +69,10 @@ $(document).ready(function() {
                     $('.persondisplay').append('<p>Git Username: ' + data.mu[currentIndex].git_username);
                     $('.persondisplay').append('<p>Shoutout: ' + data.mu[currentIndex].shoutout);
                     $('.persondisplay').fadeIn("slow");
+
+                    //The timer is reset
+                    clearInterval(timerID);
+                    timerID = setInterval(moveData, 10000);
                 });
             }
         }
